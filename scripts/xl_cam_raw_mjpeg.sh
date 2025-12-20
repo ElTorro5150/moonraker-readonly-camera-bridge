2834aa353ec079c6c8ff620168a113e181c22b91
ElTorro5150 <dwelch2177@gmail.com>
2025-12-20 14:12:44 -0600

    Create xl_cam_raw_mjpeg.sh

---
+++ scripts/xl_cam_raw_mjpeg.sh
@@ -0,0 +1,6 @@
+[43:12:25.209645151] [24083]  INFO Camera camera_manager.cpp:340 libcamera v0.6.0+rpt20251202
+[43:12:25.284043279] [24087]  INFO IPAProxy ipa_proxy.cpp:180 Using tuning file /usr/share/libcamera/ipa/rpi/vc4/imx708.json
+[43:12:25.301241228] [24087]  INFO Camera camera_manager.cpp:223 Adding camera '/base/soc/i2c0mux/i2c@1/imx708@1a' for pipeline handler rpi/vc4
+[43:12:25.301339353] [24087]  INFO RPI vc4.cpp:445 Registered camera /base/soc/i2c0mux/i2c@1/imx708@1a to Unicam device /dev/media3 and ISP device /dev/media0
+[43:12:25.301408103] [24087]  INFO RPI pipeline_base.cpp:1111 Using configuration file '/usr/share/libcamera/pipeline/rpi/vc4/rpi_apps.yaml'
+ERROR: *** failed to bind listen socket ***
